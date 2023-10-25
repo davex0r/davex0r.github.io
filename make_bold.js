064c050aeb6689c94a8ceb43a0d6d7c76b59495c
davex0r <dave.xor@gmail.com>
2023-10-25 15:29:34 +0100

    Make bold

---
+++ make_bold.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    let contentDiv = document.getElementById("content");
+    let text = contentDiv.innerHTML;
+
+    // Replace the word "sample" with "<strong>sample</strong>"
+    let newText = text.replace(/David Jordan/g, "<strong>$&</strong>");
+
+    contentDiv.innerHTML = newText;
+}
