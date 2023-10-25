beeafccde47879dc91ca32fe435d6594528622d8
davex0r <dave.xor@gmail.com>
2023-10-25 15:38:11 +0100

    Remove make_bold

--- make_bold.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    let contentDiv = document.getElementById("content");
-    let text = contentDiv.innerHTML;
-
-    // Replace the word "sample" with "<strong>sample</strong>"
-    let newText = text.replace(/David/g, "<strong>$&</strong>");
-
-    contentDiv.innerHTML = newText;
-}
